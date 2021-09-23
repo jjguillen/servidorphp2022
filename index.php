7026a3282293a4eebab466bcaae9976f0d3956cd
jjguillen <jjavierguillen@gmail.com>
2021-09-23 17:05:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+  <head> 
+    <meta charset="UTF-8"> 
+  <title></title> 
+  </head> 
+  <body> 
+   <?php 
+    echo "Bienvenido al maravilloso mundo de PHP</br>"; 
+    echo "<a href='test.php'>Test</a>"; 
+    phpinfo();
+   ?> 
+  </body> 
+</html>
