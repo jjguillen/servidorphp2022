fad08d491530119da64e42fff6296b56f127a154
jjguillen <jjavierguillen@gmail.com>
2021-09-24 11:36:33 +0200

    ej1

--- tema1/eje1.php
+++ tema1/eje1.php
@@ -5,12 +5,10 @@
       <div class="col-md-8 themed-grid-col">
 		<div class="flex-shrink-0 p-3 bg-white">
 
-
 		
 			<?php
 				echo "Aquí se ejecutaría el código del ejercicio1";
 			?>
-
 
 
 		</div>
