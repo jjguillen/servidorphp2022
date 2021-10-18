fac75bfc5c29d0a7abd3fd7fc1df37e7d81a9260
jjguillen <jjavierguillen@gmail.com>
2021-10-18 18:20:42 +0200

    carrito

--- tema3/carrito/verCarro.php
+++ tema3/carrito/verCarro.php
@@ -50,6 +50,36 @@ include ('lib/lib.php');
 
 <main>
 
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="index.php" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>        
+
+        <div class="text-end">
+            
+            <form action="verCarro.php" method="get">
+                <button type="submit" class="btn btn-outline-light me-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                    </svg>
+                </button>
+                <label for=""><?=count($_SESSION['carrito'])?></label>
+            </form>
+        </div>
+      </div>
+    </div>
+  </header>
+      
   <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
@@ -77,9 +107,13 @@ include ('lib/lib.php');
 
 <?php
     if (isset($_SESSION['carrito'])) {
+        $total = 0;
         foreach($_SESSION['carrito'] as $key => $producto) {
             //Sacar datos de el producto con ese id
             $datos = buscar($producto['id'],$productos);
+
+            //Total de cada línea del carro. Vamos acumulando
+            $total += $producto['cant'] * $datos[1];
 
             echo "<tr>";
             echo "  <td>{$datos[0]}</td>";
@@ -100,6 +134,13 @@ include ('lib/lib.php');
 
 <?php
         }
+
+        //Calcular el total
+        echo "<tr>";
+        echo "<td>Total</td><td></td><td></td>";
+        echo "<td colspan='2'><strong>{$total}€</strong></td>";
+        echo "</tr>";
+
     }
 ?>            
             </tbody>
