74b6aa52f4071537b258aca098bd2eee15abe611
jjguillen <jjavierguillen@gmail.com>
2021-10-25 16:46:35 +0200

    email

--- tema4/email/index.php
+++ tema4/email/index.php
@@ -2,6 +2,15 @@
 $to = "jjavierguillen@gmail.com"; 
 $subject = "Ejercicio 1: Prueba de envío";
 $message = "Si lees esto es que se ha enviado correctamente"; 
-mail($to, $subject, $message); 
-echo "Enviado correctamente";
+
+$header = 'From: ' . "jjavierguillen@gmail.com" . "\r\n"; 
+$header .= 'MIME-Version: 1.0' . "\r\n"; 
+$header .= 'Content-type: text/html; charset=utf-8'."\r\n"; 
+$header .= 'X-Mailer: PHP/' . phpversion(); 
+if(mail($to, $subject, $message, $header)){ 
+echo "Mensaje enviado correctamente"; 
+}else{ 
+echo "Fallo al enviar el mensaje"; 
+}
+
 ?>
