ff2577597277e185d3ea6666e671727f49dfc991
jjguillen <jjavierguillen@gmail.com>
2021-10-25 18:04:13 +0200

    email gmail

--- tema4/email/gmail.php
+++ tema4/email/gmail.php
@@ -13,14 +13,14 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    //$mail->isSMTP();                                            //Send using SMTP
-    //$mail->Host       = 'free.mboxhosting.com';                     //Set the SMTP server to send through
-    //$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    //$mail->Username   = 'iesjaroso@iesjaroso.tk';                     //SMTP username
-    //$mail->Password   = 'mjcrlvj#21';                               //SMTP password
-    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    //$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jjavierguillen@gmail.com';                     //SMTP username
+    $mail->Password   = 'feaucnjdvdfdtakx';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('iesjaroso@iesjaroso.tk', 'Javier');
