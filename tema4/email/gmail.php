439823d96eafc6e1deafff363c65279b838fb9a9
jjguillen <jjavierguillen@gmail.com>
2021-10-25 18:07:55 +0200

    gmail con adjunto

--- tema4/email/gmail.php
+++ tema4/email/gmail.php
@@ -32,13 +32,13 @@ try {
 
     //Attachments
     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    $mail->addAttachment('./marea.jpg', 'nuevomarea.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Correo de prueba con Gmail';
+    $mail->Body    = 'Este el cuerpo del mensaje <b>ojo, viene con adjunto!</b>';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
