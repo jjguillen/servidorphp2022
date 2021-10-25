53e20f082859b89f3af5f909cde775bc8c5745fd
jjguillen <jjavierguillen@gmail.com>
2021-10-25 18:40:34 +0200

    gmail php

--- tema4/email/index.php
+++ tema4/email/index.php
@@ -13,17 +13,17 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    //$mail->isSMTP();                                            //Send using SMTP
-    //$mail->Host       = 'free.mboxhosting.com';                     //Set the SMTP server to send through
-    //$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    //$mail->Username   = 'iesjaroso@iesjaroso.tk';                     //SMTP username
-    //$mail->Password   = 'mjcrlvj#21';                               //SMTP password
-    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    //$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jjavierguillen@gmail.com';                     //SMTP username
+    $mail->Password   = 'feaucnjdvdfdtakx';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('iesjaroso@iesjaroso.tk', 'Javier');
+    $mail->setFrom('jjavierguillen@gmail.com', 'Javier');
     $mail->addAddress('jjavierguillen@gmail.com', 'ProfeJJ');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
@@ -32,13 +32,13 @@ try {
 
     //Attachments
     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    $mail->addAttachment('./marea.jpg', 'nuevomarea.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Correo de prueba con Gmail';
+    $mail->Body    = 'Este el cuerpo del mensaje <b>ojo, viene con adjunto!</b>';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
