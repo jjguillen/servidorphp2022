c7a5d4cee3d0322a1cf4b651079c637f912dec46
= <=>
2021-10-29 10:10:35 +0200

    ext-mbstring

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '540d7e3f2d479a2baf45e29b98191beda575ad6e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '540d7e3f2d479a2baf45e29b98191beda575ad6e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
