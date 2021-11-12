d9e95732dd4140c99259eac7248126cb702327b2
= <=>
2021-11-12 14:15:54 +0100

    docker

--- tema5/iesjaroso/DumpBD/iesjaroso_alumnos.sql
+++ tema5/iesjaroso/DumpBD/iesjaroso_alumnos.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `alumnos`
 --
 
+CREATE DATABASE iesjaroso;
+USE iesjaroso;
+
 DROP TABLE IF EXISTS `alumnos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
--- tema5/iesjaroso/DumpBD/iesjaroso_usuarios.sql
+++ tema5/iesjaroso/DumpBD/iesjaroso_usuarios.sql
@@ -18,6 +18,7 @@
 --
 -- Table structure for table `usuarios`
 --
+USE iesjaroso;
 
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
