846cba977ce46fb7cdbbcca29033a187eef3a8ac
jjguillen <jjavierguillen@gmail.com>
2021-11-15 17:44:09 +0100

    heroku bd

--- tema5/iesjaroso/modelo.php
+++ tema5/iesjaroso/modelo.php
@@ -7,9 +7,11 @@ function conectar() {
         //$dsn = "mysql:host=172.19.0.2;dbname=iesjaroso";            
         //$dbh = new PDO($dsn, "root", "root");
         //AwardSpace
-        $dsn = getenv("DATABASE_URL");
-        //$dsn = "mysql:host=fdb22.awardspace.net;port=3306;dbname=2872262_daw";            
-        $dbh = new PDO($dsn, "2872262_daw", "mjcrlvj#21");
+        $parts = (parse_url(getenv('DATABASE_URL') ?: 'mysql://2872262_daw:mjcrlvj#21@db22.awardspace.net:3306/2872262_daw'));
+        extract($parts);
+        $path = ltrim($path, "/");
+        $dbh = new PDO("mysql:host={$host};port={$port};dbname={$path}", "2872262_daw", "mjcrlvj#21");          
+        //$dbh = new PDO($dsn, "2872262_daw", "mjcrlvj#21");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch (PDOException $e){
         echo $e->getMessage()+" "+$dsn;
