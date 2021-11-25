de3fbf1f0cf7ec2c838c30e4c8d79cdbd3117a7d
jjguillen <jjavierguillen@gmail.com>
2021-11-25 19:12:28 +0100

    isjaroso mvc

--- tema6/iesjaroso/modelos/Parte.php
+++ tema6/iesjaroso/modelos/Parte.php
@@ -1,6 +1,7 @@
 <?php
 
     class Parte {
+        protected $id;
         protected $alumno_id;
         protected $curso_id;
         protected $fecha;
@@ -10,7 +11,8 @@
         protected $descripcion;
         protected $comunicado;
 
-        public function __construct($alumno_id="",$curso_id="",$fecha="",$hora="",$asignatura="",$gravedad="",$descripcion="",$comunicado="") {
+        public function __construct($id="",$alumno_id="",$curso_id="",$fecha="",$hora="",$asignatura="",$gravedad="",$descripcion="",$comunicado="") {
+            $this->id = $id;
             $this->alumno_id = $alumno_id;
             $this->curso_id = $curso_id;
             $this->fecha = $fecha;
@@ -39,6 +41,170 @@
         public function setGravedad($gravedad)
         {
                 $this->gravedad = $gravedad;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of alumno_id
+         */ 
+        public function getAlumno_id()
+        {
+                return $this->alumno_id;
+        }
+
+        /**
+         * Set the value of alumno_id
+         *
+         * @return  self
+         */ 
+        public function setAlumno_id($alumno_id)
+        {
+                $this->alumno_id = $alumno_id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of curso_id
+         */ 
+        public function getCurso_id()
+        {
+                return $this->curso_id;
+        }
+
+        /**
+         * Set the value of curso_id
+         *
+         * @return  self
+         */ 
+        public function setCurso_id($curso_id)
+        {
+                $this->curso_id = $curso_id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of fecha
+         */ 
+        public function getFecha()
+        {
+                return $this->fecha;
+        }
+
+        /**
+         * Set the value of fecha
+         *
+         * @return  self
+         */ 
+        public function setFecha($fecha)
+        {
+                $this->fecha = $fecha;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of hora
+         */ 
+        public function getHora()
+        {
+                return $this->hora;
+        }
+
+        /**
+         * Set the value of hora
+         *
+         * @return  self
+         */ 
+        public function setHora($hora)
+        {
+                $this->hora = $hora;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of asignatura
+         */ 
+        public function getAsignatura()
+        {
+                return $this->asignatura;
+        }
+
+        /**
+         * Set the value of asignatura
+         *
+         * @return  self
+         */ 
+        public function setAsignatura($asignatura)
+        {
+                $this->asignatura = $asignatura;
+
+                return $this;
+        }
+
+       
+
+       
+
+        /**
+         * Get the value of descripcion
+         */ 
+        public function getDescripcion()
+        {
+                return $this->descripcion;
+        }
+
+        /**
+         * Set the value of descripcion
+         *
+         * @return  self
+         */ 
+        public function setDescripcion($descripcion)
+        {
+                $this->descripcion = $descripcion;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of comunicado
+         */ 
+        public function getComunicado()
+        {
+                return $this->comunicado;
+        }
+
+        /**
+         * Set the value of comunicado
+         *
+         * @return  self
+         */ 
+        public function setComunicado($comunicado)
+        {
+                $this->comunicado = $comunicado;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
 
                 return $this;
         }
