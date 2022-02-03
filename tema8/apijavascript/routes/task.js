c10e218ec46b656d6aa727a6e152e087ad94ccaa
= <=>
2022-02-03 11:15:42 +0100

    Securizando Api

--- tema8/apijavascript/routes/task.js
+++ tema8/apijavascript/routes/task.js
@@ -4,20 +4,23 @@ const TaskController = require("../controllers/task");
 
 const api = express.Router();
 
+//Importamos middleware
+const md_auth = require("../middlewares/authenticated");
+
 //Crear tarea con POST
-api.post('/task', TaskController.createTask);
+api.post('/task',  [md_auth.ensureAuth], TaskController.createTask);
 
 //Obtener todas las tareas
-api.get('/task', TaskController.getTasks);
+api.get('/task', [md_auth.ensureAuth], TaskController.getTasks);
 
 //Obtener ruta por id
-api.get('/task/:id', TaskController.getTask);
+api.get('/task/:id',  [md_auth.ensureAuth], TaskController.getTask);
 
 //Actualizar ruta por id
-api.put('/task/:id', TaskController.updateTask);
+api.put('/task/:id', [md_auth.ensureAuth], TaskController.updateTask);
 
 //Eliminar ruta por id
-api.delete('/task/:id', TaskController.deleteTask);
+api.delete('/task/:id', [md_auth.ensureAuth], TaskController.deleteTask);
 
 
 module.exports = api;
